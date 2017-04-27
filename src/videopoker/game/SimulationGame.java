4381be3d132f8da03f1132e3ed861e52a325d030
amadilsons <joao.amado.95@gmail.com>
2017-04-27 16:04:26 +0100

    added template for simulation game

---
+++ src/videopoker/game/SimulationGame.java
@@ -0,0 +1,17 @@
+package videopoker.game;
+
+public class SimulationGame implements Game{
+
+    public SimulationGame(){
+
+    }
+
+    void betStage(){}
+
+    void dealStage(){}
+
+    void holdStage(){}
+
+    void evaluationStage(){}
+      
+}
