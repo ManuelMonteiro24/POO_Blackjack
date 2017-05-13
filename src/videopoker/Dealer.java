2ae865155730123c23fb965981a01005e6b997c7
amadilsons <joao.amado.95@gmail.com>
2017-05-13 17:27:22 +0100

    finished comments

--- src/videopoker/Dealer.java
+++ src/videopoker/Dealer.java
@@ -7,74 +7,119 @@ import videopoker.evaluators.Adviser;
 
 import java.util.ArrayList;
 
-public class Dealer extends HandEvaluator{
+/**
+ * Class used to represent the dealer in the various game modes.
+ * It has all the functionalities a dealer should have like suffling
+ * the deck or advising about the best play to make.
+ *
+ * It extends the HandEvaluator class so this all Dealer objects can have
+ * the ability to correctly evaluate any given 5 card hand.
+ */
+public class Dealer extends HandEvaluator {
 
     private final static int[] creditPayout = {0, 0, 1, 1, 3, 5, 7, 10, 50, 80, 160, 50, 250}; //one for each element of the HandRank enum
     private int rank;
     public Adviser adviser;
-    public static Deck deck;
+    public Deck deck;
 
-    public Dealer(){
+    /**
+     * Empty Dealer constructor. Initializes the instance attributes and calls its
+     * super class constructor. Returns a Dealer instance with a full 52 card deck.
+     */
+    public Dealer() {
         super();
         deck = new Deck();
         this.adviser = new Adviser();
     }
 
-    public Dealer(ArrayList<Card> customDeck){
+    /**
+     * Constructor that does the same initializations as first one with the exception
+     * that it instanciates a Dealer object with a custom Deck containing only the cards @customDeck.
+     *
+     * @param customDeck ArrayList of Card objects to initialize the deck @deck with.
+     */
+    public Dealer(ArrayList<Card> customDeck) {
         super();
         deck = new Deck(customDeck);
         this.adviser = new Adviser();
     }
 
-    public void receiveCards(Card[] usedcards){
+    /**
+     * Returns the cards at @usedCards to the bottom of the deck.
+     *
+     * @param usedcards cards to be returned to the deck.
+     */
+    public void receiveCards(Card[] usedcards) {
         deck.receiveCards(usedcards);
     }
 
-    public void shuffleDeck(){
+    /**
+     * Shuffles the deck @deck.
+     */
+    public void shuffleDeck() {
         deck.shuffle();
     }
 
-    public Hand dealFullHand(){
+    /**
+     * Deals a full hand of five cards returning the corresponding Hand object.
+     *
+     * @return Hand object representing a full five Card hand.
+     */
+    public Hand dealFullHand() {
         Card[] iniCards = new Card[5];
 
-        for(int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++)
             iniCards[i] = deck.draw();
 
         Hand playersHand = new Hand(iniCards);
         return playersHand;
     }
 
-    public Card[] dealSecondCards(int nbCards){
+    /**
+     * Used when the there is a need to deal more cards to the player after the first deal.
+     * It deals only the number of cards specified by @nbCards.
+     *
+     * @param nbCards number of cards to be dealt.
+     * @return Card array with the cards to be dealt.
+     */
+    public Card[] dealSecondCards(int nbCards) {
         Card[] iniCards = new Card[nbCards];
 
-        for(int i = 0; i < iniCards.length; i++)
+        for (int i = 0; i < iniCards.length; i++)
             iniCards[i] = deck.draw();
 
         return iniCards;
     }
 
-    public int[] getAdvice(){
+    public int[] getAdvice() {
         return this.adviser.getAdvice();
     }
 
-    //returns money for player consoante a sua mao
-    public int payout(int playerbet){
+    /**
+     * Calculates the players payout according to the bet placed at the beggining of
+     * the round in the bet stage and the player's hand rank. This method uses the multipliers
+     * form @creditPayout to calculate the final payout.
+     *
+     * @param playerbet bet placed by the player at the bet stage of the game.
+     * @return amount the player should receive.
+     */
+    public int payout(int playerbet) {
 
         HandRank[] plays = HandRank.values();
         for (int i = 0; i < plays.length; i++)
-            if (this.handRank == plays[i])
+            if (handRank == plays[i])
                 return creditPayout[i] * playerbet;
 
         return 0;
     }
 
-    //check if deck is empty (DebugGame termination)
-    public boolean checkEmptyDeck(){
-      return this.deck.isEmpty();
+    /**
+     * Checks if the @deck Card array is empty.
+     *
+     * @return true if condition holds, false otherwise
+     */
+    public boolean checkEmptyDeck() {
+        return this.deck.isEmpty();
     }
-
-    /*public String printDeck() {
-        return this.deck.toString();
-    }*/
 
 }
--- src/videopoker/utils/CommandHandler.java
+++ src/videopoker/utils/CommandHandler.java
@@ -9,6 +9,15 @@ import java.io.InputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+/**
+ * Class used to process, retrieve and validate commands for the
+ * videopoker game.
+ *
+ * This class can read commands from the standard input or from a text file.
+ * It uses regexs to allow for more flexible valid command inputs.
+ * It has methods for command retrieval and command validation according
+ * to the regexs specified in @regexs.
+ */
 public class CommandHandler{
 
     Scanner scanner;
@@ -16,6 +25,13 @@ public class CommandHandler{
     private Pattern[] patterns;
     private String command;
 
+    /**
+     * CommandHandler constructor that instanciates this class with the ability to
+     * read from the received InputStream @input. This is used by the InteractiveGame
+     * class that needs to read the commands from the System.in standard input.
+     *
+     * @param input InputStream from where the commands will be read.
+     */
     public CommandHandler(InputStream input){
         this.scanner = new Scanner(input);
 
@@ -24,6 +40,13 @@ public class CommandHandler{
             patterns[i] = Pattern.compile(regexs[i]);
     }
 
+    /**
+     * CommandHandler constructor that instanciates this class with the ability to
+     * read from the file referenced by @cmdFile. This is used by the DebugGame class
+     * that requires commands to read from a file.
+     *
+     * @param cmdFile File instance referencing the file with the commands to be used.
+     */
     public CommandHandler(File cmdFile){
         try {
             this.scanner = new Scanner(cmdFile);
@@ -38,6 +61,15 @@ public class CommandHandler{
 
     }
 
+    /**
+     * Retrieve command from input. The input from which the commands will be read can be
+     * different depending on the constructor used.
+     * The class of the object from which this method is being called is necessary because
+     * it will determine how the commands will be read.
+     *
+     * @param callerClass the class of the object from which this method is being called.
+     * @return command
+     */
     public String getCommand(Class callerClass){
 
         if(callerClass.equals(InteractiveGame.class))
@@ -65,6 +97,14 @@ public class CommandHandler{
         }
     }
 
+    /**
+     * Command validation through the regular expressions @regexs. It tries to match the @cmd
+     * with any of the regexs from @regexs. If a match is found, the index of the regex from @regexs
+     * that the command was matched against, is returned. Else and invalid index is returned.
+     *
+     * @param cmd String with the command line.
+     * @return indentification number for the command @cmd according to which @regexs element it was matched against.
+     */
     public int validateCommand(String cmd){
         int match = -1;
         boolean found = false;
